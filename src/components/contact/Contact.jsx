27a931b601f757e5845655d04d2389a6efb0f2a1
LeticiaRossett <leticiarossett@outlook.com>
2021-12-31 22:12:51 -0800

    darkmode and pic

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,21 +3,29 @@ import GitHub from "../../img/github.png";
 import LinkedIn from "../../img/linkedin.png";
 import YouTube from "../../img/youtube.png";
 import { useRef } from "react";
-import { saveAs } from "react";
+import SaveFile from './SaveFile';
+import emailjs from 'emailjs-com';
+import { useState } from "react";
 
 function Contact() {
     const formRef = useRef();
+    const [done, setDone] = useState(false);
 
-    function handleSubmit(e) {
+    const handleSubmit = (e) => {
         e.preventDefault();
-
-    };
-
-    function handleSave() {
-        saveAs(
-            "",
-            "LeticiaRossett-Resume.pdf"
-        );
+        emailjs
+            .sendForm(
+                'service_extdlab',
+                'template_4kyhibx',
+                formRef.current,
+                'user_QqOUxUkysukRXid44bbr'
+            )
+            .then((result) => {
+                console.log(result.text);
+                setDone(true);
+            }, (error) => {
+                console.log(error.text);
+            });
     };
 
     return (
@@ -30,35 +38,37 @@ function Contact() {
                         <div className="c-info-item">
                             <a href="https://www.linkedin.com/in/leticia-rossett/" target="_blank" rel="noreferrer">
                                 <img src={LinkedIn} alt="" className="c-icon" />
+                                LinkedIn
                             </a>
-                            LinkedIn
                         </div>
                         <div className="c-info-item">
                             <a href="https://github.com/LeticiaRossett" target="_blank" rel="noreferrer">
                                 <img src={GitHub} alt="" className="c-icon" />
+                                GitHub
                             </a>
-                            GitHub
                         </div>
                         <div className="c-info-item">
                             <a href="https://www.youtube.com/channel/UC18sQ-TzrjX1cnSVJ6eODRQ" target="_blank" rel="noreferrer">
                                 <img src={YouTube} alt="" className="c-icon" />
+                                YouTube
                             </a>
-                            YouTube
                         </div>
                     </div>
                 </div>
                 <div className="c-right">
                     <p className="c-desc">
-                        <b>More Info?</b> Contact me! My resume is available for download below.
+                        <b>More Info?</b> Contact me! My resume is also available
+                        for download below.
                     </p>
-
                     <form ref={formRef} onSubmit={handleSubmit}>
                         <input type='text' placeholder='Name' name='user_name' />
-                        <input type='text' placeholder='Subject' name='subject' />
+                        <input type='text' placeholder='Subject' name='user_subject' />
                         <input type='text' placeholder='Email' name='user_email' />
                         <textarea rows='5' placeholder='Message' name='message' />
                         <button>Submit</button>
+                        {done && "Thank you!"}
                     </form>
+                    <div className='c-save-button'><SaveFile /></div>
                 </div>
             </div>
         </div>
---
+++ src/components/contact/SaveFile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <div>
+
+            <a
+                href="https://github.com/LeticiaRossett/portfolio/raw/main/src/img/Leticia_Rossett_Resume.pdf"
+                download
+            >
+
+                <button>
+                    Download Resume
+                </button>
+            </a>
+        </div>
+    );
+}
+

761af1ce041cf5b778d923d5aded99594a7382a2
LeticiaRossett <leticiarossett@outlook.com>
2021-12-31 18:38:58 -0800

    cropped pictures

--- src/data.js
+++ src/data.js
@@ -2,19 +2,19 @@ export const projects = [
     {
         id: 1,
         title: 'PiPlayer',
-        img: "https://github.com/LeticiaRossett/portfolio/blob/main/src/img/arduino.png?raw=true",
+        img: "https://github.com/LeticiaRossett/portfolio/blob/main/src/img/piplayer.png?raw=true",
         link: 'https://youtu.be/kzgRUfafP10'
     },
     {
         id: 2,
         title: 'Equity Portfolio',
-        img: "https://github.com/LeticiaRossett/portfolio/blob/main/src/img/arduino.png?raw=true",
+        img: "https://github.com/LeticiaRossett/portfolio/blob/main/src/img/equityportfolio.png?raw=true",
         link: 'https://youtu.be/wPrHO8olmH8'
     },
     {
         id: 3,
         title: 'Testing Skills Website',
-        img: "https://github.com/LeticiaRossett/portfolio/blob/main/src/img/arduino.png?raw=true",
+        img: "https://github.com/LeticiaRossett/portfolio/blob/main/src/img/fp.png?raw=true",
         link: 'https://youtu.be/lzhIeD7ED4I'
     },
     {

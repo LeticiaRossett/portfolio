146ed8d9434c6d949f07e87fc714180b26805f3e
LeticiaRossett <leticiarossett@outlook.com>
2021-12-31 18:29:26 -0800

    added pic files

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -6,7 +6,7 @@ function Projects({ title, img, link }) {
         <div className='p'>
             <h3 className='p-picture-title'>{title}</h3>
             <a href={link} target="_blank" rel="noreferrer">
-                <img src={process.env.PUBLIC_URL`${img}`} alt={title} className='p-img' />
+                <img src={img} alt={title} className='p-img' />
             </a>
         </div>
     )
--- src/data.js
+++ src/data.js
@@ -2,25 +2,25 @@ export const projects = [
     {
         id: 1,
         title: 'PiPlayer',
-        img: "../../img/piplayer.png",
+        img: "https://github.com/LeticiaRossett/portfolio/blob/main/src/img/arduino.png?raw=true",
         link: 'https://youtu.be/kzgRUfafP10'
     },
     {
         id: 2,
         title: 'Equity Portfolio',
-        img: "../../img/equity_portfolio.png",
+        img: "https://github.com/LeticiaRossett/portfolio/blob/main/src/img/arduino.png?raw=true",
         link: 'https://youtu.be/wPrHO8olmH8'
     },
     {
         id: 3,
         title: 'Testing Skills Website',
-        img: "../../img/website_fp.png",
+        img: "https://github.com/LeticiaRossett/portfolio/blob/main/src/img/arduino.png?raw=true",
         link: 'https://youtu.be/lzhIeD7ED4I'
     },
     {
         id: 4,
         title: 'Score Board with Arduino',
-        img: "../../img/arduino.png",
+        img: "https://github.com/LeticiaRossett/portfolio/blob/main/src/img/arduino.png?raw=true",
         link: 'https://youtu.be/hO794G5yweg'
     }
 ]
